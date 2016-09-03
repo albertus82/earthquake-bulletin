5b1675d61905b848f563afdbfe2908220d3a7b65
Albertus82 <albertus82@gmail.com>
2016-09-03 21:24:53 +0200

    Status.

---
+++ src/main/java/it/albertus/geofon/client/model/Status.java
@@ -0,0 +1,19 @@
+package it.albertus.geofon.client.model;
+
+public enum Status {
+
+	A("automatic"),
+	C("confirmed"),
+	M("manually revised");
+
+	private final String description;
+
+	Status(final String description) {
+		this.description = description;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+}
