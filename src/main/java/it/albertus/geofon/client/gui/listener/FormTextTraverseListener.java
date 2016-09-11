ff46f6df6ff4f956edfa01948972c639b7f62cfe
Albertus82 <albertus82@gmail.com>
2016-09-11 23:02:37 +0200

    MessageBox.

--- src/main/java/it/albertus/geofon/client/gui/listener/FormTextTraverseListener.java
+++ src/main/java/it/albertus/geofon/client/gui/listener/FormTextTraverseListener.java
@@ -1,10 +1,12 @@
 package it.albertus.geofon.client.gui.listener;
 
 import it.albertus.geofon.client.gui.SearchForm;
+import it.albertus.geofon.client.resources.Messages;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.TraverseEvent;
 import org.eclipse.swt.events.TraverseListener;
+import org.eclipse.swt.widgets.MessageBox;
 
 public class FormTextTraverseListener implements TraverseListener {
 
@@ -17,7 +19,15 @@ public class FormTextTraverseListener implements TraverseListener {
 	@Override
 	public void keyTraversed(final TraverseEvent te) {
 		if (te.detail == SWT.TRAVERSE_RETURN) {
-			form.getSearchButton().notifyListeners(SWT.Selection, null);
+			if (form.getSearchButton().isEnabled()) {
+				form.getSearchButton().notifyListeners(SWT.Selection, null);
+			}
+			else {
+				final MessageBox dialog = new MessageBox(form.getFormComposite().getShell(), SWT.ICON_INFORMATION);
+				dialog.setText(Messages.get("lbl.window.title"));
+				dialog.setMessage(Messages.get("msg.form.search.ongoing.message"));
+				dialog.open();
+			}
 		}
 	}
 
