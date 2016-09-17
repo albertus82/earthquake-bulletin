914c84c7e31e069702028d23752cf110bb15ecac
Albertus82 <albertus82@gmail.com>
2016-09-17 14:16:38 +0200

    Coordinates fix.

--- src/main/java/it/albertus/earthquake/xhtml/transformer/XhtmlTableDataTransformer.java
+++ src/main/java/it/albertus/earthquake/xhtml/transformer/XhtmlTableDataTransformer.java
@@ -27,13 +27,19 @@ public class XhtmlTableDataTransformer {
 	private static final String timeSuffix = "</a";
 	private static final String magnitudePrefix = ">";
 	private static final String magnitudeSuffix = "</";
-	private static final String coordinatesPrefix = magnitudePrefix;
-	private static final String coordinatesSuffix = "&deg;";
+	private static final String latitudePrefix = magnitudePrefix;
+	private static final String latitudeSuffix = "&deg;";
+	private static final String longitudePrefix = latitudePrefix;
+	private static final String longitudeSuffix = latitudeSuffix;
 	private static final String depthSuffix = magnitudeSuffix;
 	private static final String statusPrefix = guidSuffix;
 	private static final String statusSuffix = magnitudeSuffix;
 	private static final String regionPrefix = statusPrefix;
 	private static final String regionSuffix = magnitudeSuffix;
+	private static final String latitudeSignPrefix = latitudeSuffix;
+	private static final String longitudeSignPrefix = latitudeSuffix;
+	private static final String latitudeSignSuffix = magnitudeSuffix;
+	private static final String longitudeSignSuffix = magnitudeSuffix;
 
 	/** Use {@link #parseRssDate} method instead. */
 	@Deprecated
@@ -69,17 +75,34 @@ public class XhtmlTableDataTransformer {
 	private static Earthquake fromXhtml(final String td) throws IllegalArgumentException {
 		try {
 			final String lines[] = td.split(NewLine.SYSTEM_LINE_SEPARATOR);
+
 			final Calendar time = Calendar.getInstance();
 			time.setTime(parseXhtmlDate(lines[0].substring(lines[0].lastIndexOf(timePrefix) + timePrefix.length(), lines[0].indexOf(timeSuffix)).trim()));
+
 			final String guid = lines[0].substring(lines[0].indexOf(guidPrefix) + guidPrefix.length(), lines[0].lastIndexOf(guidSuffix)).trim();
+
 			final float magnitude = Float.parseFloat(lines[1].substring(lines[1].indexOf(magnitudePrefix) + magnitudePrefix.length(), lines[1].indexOf(magnitudeSuffix)).trim());
-			final float latitude = Float.parseFloat(lines[2].substring(lines[2].indexOf(coordinatesPrefix) + coordinatesPrefix.length(), lines[2].indexOf(coordinatesSuffix)).trim());
-			final float longitude = Float.parseFloat(lines[3].substring(lines[3].indexOf(coordinatesPrefix) + coordinatesPrefix.length(), lines[3].indexOf(coordinatesSuffix)).trim());
+
+			float latitude = Float.parseFloat(lines[2].substring(lines[2].indexOf(latitudePrefix) + latitudePrefix.length(), lines[2].indexOf(latitudeSuffix)).trim());
+			if ("S".equalsIgnoreCase(lines[2].substring(lines[2].indexOf(latitudeSignPrefix) + latitudeSignPrefix.length(), lines[2].indexOf(latitudeSignSuffix)).trim())) {
+				latitude *= -1;
+			}
+
+			float longitude = Float.parseFloat(lines[3].substring(lines[3].indexOf(longitudePrefix) + longitudePrefix.length(), lines[3].indexOf(longitudeSuffix)).trim());
+			if ("W".equalsIgnoreCase(lines[3].substring(lines[3].indexOf(longitudeSignPrefix) + longitudeSignPrefix.length(), lines[3].indexOf(longitudeSignSuffix)).trim())) {
+				longitude *= -1;
+			}
+
 			final int depth = Integer.parseInt(lines[4].substring(lines[4].indexOf(magnitudePrefix) + magnitudePrefix.length(), lines[4].indexOf(depthSuffix)).trim());
+
 			final Status status = Status.valueOf(lines[5].substring(lines[5].lastIndexOf(statusPrefix) + statusPrefix.length(), lines[5].indexOf(statusSuffix)).trim());
+
 			final String region = lines[6].substring(lines[6].lastIndexOf(regionPrefix) + regionPrefix.length(), lines[6].lastIndexOf(regionSuffix)).trim();
+
 			final URL link = new URL(EarthquakeBulletin.BASE_URL + "/eqinfo/event.php?id=" + guid);
+
 			final URL enclosure = new URL(EarthquakeBulletin.BASE_URL + "/data/alerts/" + time.get(Calendar.YEAR) + "/" + guid + "/" + guid + ".jpg");
+
 			return new Earthquake(guid, time.getTime(), magnitude, new Latitude(latitude), new Longitude(longitude), new Depth(depth), status, region, link, enclosure);
 		}
 		catch (final Exception e) {
