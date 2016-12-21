96fc31bddbccadb3c00b0bcf11d38b9387426862
Albertus82 <albertus82@gmail.com>
2016-12-21 23:03:54 +0100

    Improved search job.

--- src/main/java/it/albertus/earthquake/gui/job/SearchJob.java
+++ src/main/java/it/albertus/earthquake/gui/job/SearchJob.java
@@ -12,6 +12,7 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.TreeSet;
+import java.util.zip.GZIPInputStream;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.Unmarshaller;
@@ -38,6 +39,7 @@ import it.albertus.earthquake.resources.Messages;
 import it.albertus.earthquake.rss.transformer.RssItemTransformer;
 import it.albertus.earthquake.rss.xml.Rss;
 import it.albertus.jface.SwtThreadExecutor;
+import it.albertus.util.IOUtils;
 import it.albertus.util.NewLine;
 
 public class SearchJob extends Job {
@@ -52,187 +54,256 @@ public class SearchJob extends Job {
 		this.setUser(true);
 	}
 
+	private class JobVariables {
+		private boolean formValid;
+		private long waitTimeInMillis;
+		private Format format;
+		private boolean error;
+		private final Map<String, String> params = new LinkedHashMap<>();
+
+		public boolean isFormValid() {
+			return formValid;
+		}
+
+		public void setFormValid(boolean formValid) {
+			this.formValid = formValid;
+		}
+
+		public long getWaitTimeInMillis() {
+			return waitTimeInMillis;
+		}
+
+		public void setWaitTimeInMillis(long waitTimeInMillis) {
+			this.waitTimeInMillis = waitTimeInMillis;
+		}
+
+		public Format getFormat() {
+			return format;
+		}
+
+		public void setFormat(Format format) {
+			this.format = format;
+		}
+
+		public boolean isError() {
+			return error;
+		}
+
+		public void setError(boolean error) {
+			this.error = error;
+		}
+
+		public Map<String, String> getParams() {
+			return params;
+		}
+	}
+
 	@Override
 	protected IStatus run(final IProgressMonitor monitor) {
-		monitor.beginTask("Search", 1);
-
-		boolean error = false;
-
-		final long[] waitTimeInMillis = new long[1];
-
-		final Format[] format = new Format[] { SearchForm.Defaults.FORMAT };
-
-		final Map<String, String> params = new LinkedHashMap<>();
-
-		new SwtThreadExecutor(gui.getShell()) {
-			@Override
-			protected void run() {
-				gui.getSearchForm().updateButtons();
-				gui.getShell().setCursor(gui.getShell().getDisplay().getSystemCursor(SWT.CURSOR_WAIT));
-
-				// Parametri di ricerca
-				final SearchForm form = gui.getSearchForm();
-
-				for (final Entry<Format, Button> entry : form.getFormatRadios().entrySet()) {
-					if (entry.getValue().getSelection()) {
-						format[0] = entry.getKey();
-						break;
-					}
-				}
-				params.put("fmt", format[0].getValue());
-				params.put("mode", form.getRestrictButton().getSelection() ? "mt" : "");
-				if (form.getPeriodFromText().isEnabled()) {
-					params.put("datemin", URIEncoder.encodeURI(form.getPeriodFromText().getText()));
-				}
-				if (form.getPeriodToText().isEnabled()) {
-					params.put("datemax", URIEncoder.encodeURI(form.getPeriodToText().getText()));
-				}
-				params.put("latmin", URIEncoder.encodeURI(form.getLatitudeFromText().getText()));
-				params.put("latmax", URIEncoder.encodeURI(form.getLatitudeToText().getText()));
-				params.put("lonmin", URIEncoder.encodeURI(form.getLongitudeFromText().getText()));
-				params.put("lonmax", URIEncoder.encodeURI(form.getLongitudeToText().getText()));
-				params.put("magmin", URIEncoder.encodeURI(form.getMinimumMagnitudeText().getText()));
-				params.put("nmax", URIEncoder.encodeURI(form.getResultsText().getText()));
-
-				if (gui.getSearchForm().getAutoRefreshButton().getSelection()) {
-					try {
-						short waitTimeInMinutes = Short.parseShort(gui.getSearchForm().getAutoRefreshText().getText());
-						if (waitTimeInMinutes > 0) {
-							waitTimeInMillis[0] = waitTimeInMinutes * 1000 * 60;
-							gui.getSearchForm().getStopButton().setEnabled(true);
-						}
-					}
-					catch (final RuntimeException re) {/* Ignore */}
-				}
-			}
-		}.start();
-
-		final StringBuilder urlSb = new StringBuilder(EarthquakeBulletin.BASE_URL).append("/eqinfo/list.php?fmt=").append(params.get("fmt"));
-		for (final Entry<String, String> param : params.entrySet()) {
-			if (param.getValue() != null && !param.getValue().isEmpty() && !param.getKey().equals("fmt")) {
-				urlSb.append("&").append(param.getKey()).append("=").append(param.getValue());
-			}
-		}
-
-		Rss rss = null;
-		TableData td = null;
-		InputStream is = null;
 		try {
-			final URL url = new URL(urlSb.toString());
-			final HttpURLConnection urlConnection = openConnection(url);
-			is = urlConnection.getInputStream();
-			switch (format[0]) {
-			case RSS:
-				final JAXBContext jaxbContext = JAXBContext.newInstance(Rss.class);
-				final Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
-				rss = (Rss) jaxbUnmarshaller.unmarshal(is);
-				urlConnection.disconnect();
-				break;
-			case HTML:
-				td = new TableData();
-				try (final BufferedReader br = new BufferedReader(new InputStreamReader(is))) {
-					String line = null;
-					while ((line = br.readLine()) != null) {
-						if (line.trim().toLowerCase().contains("<tr")) {
-							final StringBuilder block = new StringBuilder();
-							while (!(line = br.readLine()).toLowerCase().contains("</tr")) {
-								block.append(line.trim()).append(NewLine.SYSTEM_LINE_SEPARATOR);
-							}
-							td.addItem(block.toString());
-						}
-					}
-				}
-				break;
-			}
-		}
-		catch (final Exception e) {
-			e.printStackTrace();
-			error = true;
+			monitor.beginTask("Search", 1);
+
+			final JobVariables jobVariables = new JobVariables();
+
 			new SwtThreadExecutor(gui.getShell()) {
 				@Override
 				protected void run() {
-					if (gui.getTrayIcon() == null || gui.getTrayIcon().getTrayItem() == null || !gui.getTrayIcon().getTrayItem().getVisible()) {
-						final MessageBox dialog = new MessageBox(gui.getShell(), SWT.ICON_WARNING);
-						dialog.setText(Messages.get("lbl.window.title"));
-						dialog.setMessage(Messages.get("err.job.search"));
-						dialog.open();
+					final boolean formValid = gui.getSearchForm().isValid();
+					jobVariables.setFormValid(formValid);
+					if (formValid) {
+						gui.getSearchForm().getStopButton().notifyListeners(SWT.Selection, null);
 					}
 				}
 			}.start();
-		}
-		finally {
-			try {
-				is.close();
-			}
-			catch (final Exception e) {/* Ignore */}
-		}
 
-		final Collection<Earthquake> earthquakes = new TreeSet<>();
-		if (!error) {
-			try {
-				switch (format[0]) {
-				case RSS:
-					earthquakes.addAll(RssItemTransformer.fromRss(rss));
-					break;
-				case HTML:
-					earthquakes.addAll(HtmlTableDataTransformer.fromHtml(td));
-					break;
-				}
-			}
-			catch (final Exception e) {
-				error = true;
-				e.printStackTrace();
+			if (jobVariables.isFormValid()) {
+				jobVariables.setFormat(SearchForm.Defaults.FORMAT);
+
 				new SwtThreadExecutor(gui.getShell()) {
 					@Override
 					protected void run() {
-						if (gui.getTrayIcon() == null || gui.getTrayIcon().getTrayItem() == null || !gui.getTrayIcon().getTrayItem().getVisible()) {
-							final MessageBox dialog = new MessageBox(gui.getShell(), SWT.ICON_WARNING);
-							dialog.setText(Messages.get("lbl.window.title"));
-							dialog.setMessage(Messages.get("err.job.decode"));
-							dialog.open();
+						gui.getSearchForm().updateButtons();
+						gui.getShell().setCursor(gui.getShell().getDisplay().getSystemCursor(SWT.CURSOR_WAIT));
+
+						// Parametri di ricerca
+						final SearchForm form = gui.getSearchForm();
+
+						for (final Entry<Format, Button> entry : form.getFormatRadios().entrySet()) {
+							if (entry.getValue().getSelection()) {
+								jobVariables.setFormat(entry.getKey());
+								break;
+							}
+						}
+						final Map<String, String> params = jobVariables.getParams();
+						params.put("fmt", jobVariables.getFormat().getValue());
+						params.put("mode", form.getRestrictButton().getSelection() ? "mt" : "");
+						if (form.getPeriodFromText().isEnabled()) {
+							params.put("datemin", URIEncoder.encodeURI(form.getPeriodFromText().getText()));
+						}
+						if (form.getPeriodToText().isEnabled()) {
+							params.put("datemax", URIEncoder.encodeURI(form.getPeriodToText().getText()));
+						}
+						params.put("latmin", URIEncoder.encodeURI(form.getLatitudeFromText().getText()));
+						params.put("latmax", URIEncoder.encodeURI(form.getLatitudeToText().getText()));
+						params.put("lonmin", URIEncoder.encodeURI(form.getLongitudeFromText().getText()));
+						params.put("lonmax", URIEncoder.encodeURI(form.getLongitudeToText().getText()));
+						params.put("magmin", URIEncoder.encodeURI(form.getMinimumMagnitudeText().getText()));
+						params.put("nmax", URIEncoder.encodeURI(form.getResultsText().getText()));
+
+						if (gui.getSearchForm().getAutoRefreshButton().getSelection()) {
+							try {
+								short waitTimeInMinutes = Short.parseShort(gui.getSearchForm().getAutoRefreshText().getText());
+								if (waitTimeInMinutes > 0) {
+									jobVariables.setWaitTimeInMillis(waitTimeInMinutes * 1000 * 60);
+									gui.getSearchForm().getStopButton().setEnabled(true);
+								}
+							}
+							catch (final RuntimeException re) {/* Ignore */}
 						}
 					}
 				}.start();
-			}
-		}
 
-		if (!error) {
-			new SwtThreadExecutor(gui.getShell()) {
-				@Override
-				protected void run() {
-					final Earthquake[] newData = earthquakes.toArray(new Earthquake[0]);
-					final Earthquake[] oldData = (Earthquake[]) gui.getResultsTable().getTableViewer().getInput();
-					gui.getResultsTable().getTableViewer().setInput(newData);
-					if (oldData != null && !Arrays.equals(newData, oldData)) {
-						gui.getMapCanvas().clear();
-						if (newData.length > 0 && newData[0] != null && oldData.length > 0 && !newData[0].equals(oldData[0])) {
-							gui.getTrayIcon().showBalloonToolTip(newData[0]);
-						}
+				final StringBuilder urlSb = new StringBuilder(EarthquakeBulletin.BASE_URL).append("/eqinfo/list.php?fmt=").append(jobVariables.getParams().get("fmt"));
+				for (final Entry<String, String> param : jobVariables.getParams().entrySet()) {
+					if (param.getValue() != null && !param.getValue().isEmpty() && !param.getKey().equals("fmt")) {
+						urlSb.append("&").append(param.getKey()).append("=").append(param.getValue());
 					}
 				}
-			}.start();
-		}
 
-		new SwtThreadExecutor(gui.getShell()) {
-			@Override
-			protected void run() {
-				if (waitTimeInMillis[0] > 0) {
-					schedule(waitTimeInMillis[0]);
+				Rss rss = null;
+				TableData td = null;
+				InputStream innerStream = null;
+				InputStream wrapperStream = null;
+				try {
+					final URL url = new URL(urlSb.toString());
+					final HttpURLConnection urlConnection = openConnection(url);
+					final String responseContentEncoding = urlConnection.getHeaderField("Content-Encoding");
+					final boolean gzip = responseContentEncoding != null && responseContentEncoding.toLowerCase().contains("gzip");
+					innerStream = urlConnection.getInputStream();
+					if (gzip) {
+						wrapperStream = new GZIPInputStream(innerStream);
+					}
+					else {
+						wrapperStream = innerStream;
+					}
+					switch (jobVariables.getFormat()) {
+					case RSS:
+						final JAXBContext jaxbContext = JAXBContext.newInstance(Rss.class);
+						final Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+						rss = (Rss) jaxbUnmarshaller.unmarshal(wrapperStream);
+						urlConnection.disconnect();
+						break;
+					case HTML:
+						td = new TableData();
+						try (final BufferedReader br = new BufferedReader(new InputStreamReader(wrapperStream))) {
+							String line = null;
+							while ((line = br.readLine()) != null) {
+								if (line.trim().toLowerCase().contains("<tr")) {
+									final StringBuilder block = new StringBuilder();
+									while (!(line = br.readLine()).toLowerCase().contains("</tr")) {
+										block.append(line.trim()).append(NewLine.SYSTEM_LINE_SEPARATOR);
+									}
+									td.addItem(block.toString());
+								}
+							}
+						}
+						break;
+					}
 				}
-				else {
-					gui.getSearchForm().getStopButton().notifyListeners(SWT.Selection, null);
+				catch (final Exception e) {
+					e.printStackTrace();
+					jobVariables.setError(true);
+					new SwtThreadExecutor(gui.getShell()) {
+						@Override
+						protected void run() {
+							if (gui.getTrayIcon() == null || gui.getTrayIcon().getTrayItem() == null || !gui.getTrayIcon().getTrayItem().getVisible()) {
+								final MessageBox dialog = new MessageBox(gui.getShell(), SWT.ICON_WARNING);
+								dialog.setText(Messages.get("lbl.window.title"));
+								dialog.setMessage(Messages.get("err.job.search"));
+								dialog.open();
+							}
+						}
+					}.start();
 				}
-				gui.getShell().setCursor(null);
+				finally {
+					IOUtils.closeQuietly(wrapperStream);
+					IOUtils.closeQuietly(innerStream);
+				}
+
+				final Collection<Earthquake> earthquakes = new TreeSet<>();
+				if (!jobVariables.isError()) {
+					try {
+						switch (jobVariables.getFormat()) {
+						case RSS:
+							earthquakes.addAll(RssItemTransformer.fromRss(rss));
+							break;
+						case HTML:
+							earthquakes.addAll(HtmlTableDataTransformer.fromHtml(td));
+							break;
+						}
+					}
+					catch (final Exception e) {
+						jobVariables.setError(true);
+						e.printStackTrace();
+						new SwtThreadExecutor(gui.getShell()) {
+							@Override
+							protected void run() {
+								if (gui.getTrayIcon() == null || gui.getTrayIcon().getTrayItem() == null || !gui.getTrayIcon().getTrayItem().getVisible()) {
+									final MessageBox dialog = new MessageBox(gui.getShell(), SWT.ICON_WARNING);
+									dialog.setText(Messages.get("lbl.window.title"));
+									dialog.setMessage(Messages.get("err.job.decode"));
+									dialog.open();
+								}
+							}
+						}.start();
+					}
+				}
+
+				if (!jobVariables.isError()) {
+					new SwtThreadExecutor(gui.getShell()) {
+						@Override
+						protected void run() {
+							final Earthquake[] newData = earthquakes.toArray(new Earthquake[0]);
+							final Earthquake[] oldData = (Earthquake[]) gui.getResultsTable().getTableViewer().getInput();
+							gui.getResultsTable().getTableViewer().setInput(newData);
+							if (oldData != null && !Arrays.equals(newData, oldData)) {
+								gui.getMapCanvas().clear();
+								if (newData.length > 0 && newData[0] != null && oldData.length > 0 && !newData[0].equals(oldData[0])) {
+									gui.getTrayIcon().showBalloonToolTip(newData[0]);
+								}
+							}
+						}
+					}.start();
+				}
+
+				new SwtThreadExecutor(gui.getShell()) {
+					@Override
+					protected void run() {
+						final long waitTimeInMillis = jobVariables.getWaitTimeInMillis();
+						if (waitTimeInMillis > 0) {
+							schedule(waitTimeInMillis);
+						}
+						else {
+							gui.getSearchForm().getStopButton().notifyListeners(SWT.Selection, null);
+						}
+						gui.getShell().setCursor(null);
+					}
+				}.start();
 			}
-		}.start();
-
-		monitor.done();
-		return Status.OK_STATUS;
+			monitor.done();
+			return Status.OK_STATUS;
+		}
+		catch (final RuntimeException re) {
+			re.printStackTrace();
+			throw re;
+		}
 	}
 
 	private HttpURLConnection openConnection(final URL url) throws IOException {
 		final HttpURLConnection urlConnection = HttpConnector.openConnection(url);
-		urlConnection.addRequestProperty("Accept", "*/xml");
+		urlConnection.addRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:50.0) Gecko/20100101 Firefox/50.0");
+		urlConnection.addRequestProperty("Accept", "*/*");
+		urlConnection.addRequestProperty("Accept-Encoding", "gzip");
 		return urlConnection;
 	}
 
