36ffe7f610394d0e6d01a51ebf830b4139fb5a6c
Alberto <albertus82@gmail.com>
2017-01-26 11:55:01 +0100

    Changed Windows file name cases.

--- src/main/scripts/earthquake-bulletin.bat
+++
@@ -1,3 +0,0 @@
-@ECHO OFF
-IF "%JAVA_HOME%" == "" START "" javaw.exe -Xms8m -Xmx32m -jar "%~dp0earthquake-bulletin.jar"
-IF NOT "%JAVA_HOME%" == "" START "" "%JAVA_HOME%\bin\javaw.exe" -Xms8m -Xmx32m -jar "%~dp0earthquake-bulletin.jar"
