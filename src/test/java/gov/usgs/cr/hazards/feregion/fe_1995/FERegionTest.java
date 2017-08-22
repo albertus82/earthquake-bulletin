6d30a5a48dc3ed97fd066cc0bdd6b7d6aa51e705
Alberto <albertus82@gmail.com>
2017-08-22 16:59:51 +0200

    feregion: test.

--- src/test/java/gov/usgs/cr/hazards/feregion/fe_1995/FERegionTest.java
+++ src/test/java/gov/usgs/cr/hazards/feregion/fe_1995/FERegionTest.java
@@ -15,15 +15,28 @@ import it.albertus.util.logging.LoggerFactory;
 
 public class FERegionTest {
 
-	private static final int STEP = 5;
-
 	private static final Logger logger = LoggerFactory.getLogger(FERegionTest.class);
 
 	@Test
-	public void test() throws IOException {
-		try (final InputStream is = getClass().getResourceAsStream(String.format("feregion_%d_%d.txt.gz", STEP, STEP)); final GZIPInputStream gzis = new GZIPInputStream(is); final InputStreamReader isr = new InputStreamReader(gzis); final BufferedReader br = new BufferedReader(isr)) {
-			for (int i = -180; i <= 180; i += STEP) {
-				for (int j = -90; j <= 90; j += STEP) {
+	public void test_5_5() throws IOException {
+		final int step = 5;
+		try (final InputStream is = getClass().getResourceAsStream(String.format("feregion_%d_%d.txt.gz", step, step)); final GZIPInputStream gzis = new GZIPInputStream(is); final InputStreamReader isr = new InputStreamReader(gzis); final BufferedReader br = new BufferedReader(isr)) {
+			for (int i = -180; i <= 180; i += step) {
+				for (int j = -90; j <= 90; j += step) {
+					final String name = FERegion.getName(i, j);
+					logger.log(Level.INFO, "{0}, {1}: {2}", new Object[] { i, j, name });
+					Assert.assertEquals(name, br.readLine());
+				}
+			}
+		}
+	}
+
+	@Test
+	public void test_3_3() throws IOException {
+		final int step = 3;
+		try (final InputStream is = getClass().getResourceAsStream(String.format("feregion_%d_%d.txt.gz", step, step)); final GZIPInputStream gzis = new GZIPInputStream(is); final InputStreamReader isr = new InputStreamReader(gzis); final BufferedReader br = new BufferedReader(isr)) {
+			for (int i = -180; i <= 180; i += step) {
+				for (int j = -90; j <= 90; j += step) {
 					final String name = FERegion.getName(i, j);
 					logger.log(Level.INFO, "{0}, {1}: {2}", new Object[] { i, j, name });
 					Assert.assertEquals(name, br.readLine());
