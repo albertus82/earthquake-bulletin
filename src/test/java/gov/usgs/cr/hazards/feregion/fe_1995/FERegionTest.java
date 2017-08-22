cdd30f85fca4b6aecd455014e59f186013556137
Alberto <albertus82@gmail.com>
2017-08-22 17:57:35 +0200

    feregion: test.

--- src/test/java/gov/usgs/cr/hazards/feregion/fe_1995/FERegionTest.java
+++ src/test/java/gov/usgs/cr/hazards/feregion/fe_1995/FERegionTest.java
@@ -4,18 +4,12 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import java.util.zip.GZIPInputStream;
 
 import org.junit.Assert;
 import org.junit.Test;
 
-import it.albertus.util.logging.LoggerFactory;
-
 public class FERegionTest {
-
-	private static final Logger logger = LoggerFactory.getLogger(FERegionTest.class);
 
 	@Test
 	public void test_5_5() throws IOException {
@@ -27,13 +21,17 @@ public class FERegionTest {
 		test(3);
 	}
 
+	@Test
+	public void test_2_2() throws IOException {
+		test(2);
+	}
+
 	private void test(final int step) throws IOException {
 		try (final InputStream is = getClass().getResourceAsStream(String.format("feregion_%d_%d.txt.gz", step, step)); final GZIPInputStream gzis = new GZIPInputStream(is); final InputStreamReader isr = new InputStreamReader(gzis); final BufferedReader br = new BufferedReader(isr)) {
 			for (int i = -180; i <= 180; i += step) {
 				for (int j = -90; j <= 90; j += step) {
 					final String name = FERegion.getName(i, j);
-					logger.log(Level.INFO, "{0}, {1}: {2}", new Object[] { i, j, name });
-					Assert.assertEquals(name, br.readLine());
+					Assert.assertEquals(i + ", " + j, name, br.readLine());
 				}
 			}
 		}
