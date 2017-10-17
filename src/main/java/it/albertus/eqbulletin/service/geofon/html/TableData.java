c47bb99ebfb88cf1b3759738137b3346fca1a811
Alberto <albertus82@gmail.com>
2017-10-17 22:40:58 +0200

    Fix Sonarcloud bugs.

--- src/main/java/it/albertus/eqbulletin/service/geofon/html/TableData.java
+++ src/main/java/it/albertus/eqbulletin/service/geofon/html/TableData.java
@@ -1,11 +1,11 @@
 package it.albertus.eqbulletin.service.geofon.html;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
 
 public class TableData {
 
-	private final LinkedList<String> items = new LinkedList<>();
+	private final List<String> items = new ArrayList<>();
 
 	public List<String> getItems() {
 		return items;
@@ -19,12 +19,12 @@ public class TableData {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((items == null) ? 0 : items.hashCode());
+		result = prime * result + items.hashCode();
 		return result;
 	}
 
 	@Override
-	public boolean equals(Object obj) {
+	public boolean equals(final Object obj) {
 		if (this == obj) {
 			return true;
 		}
@@ -34,16 +34,8 @@ public class TableData {
 		if (!(obj instanceof TableData)) {
 			return false;
 		}
-		TableData other = (TableData) obj;
-		if (items == null) {
-			if (other.items != null) {
-				return false;
-			}
-		}
-		else if (!items.equals(other.items)) {
-			return false;
-		}
-		return true;
+		final TableData other = (TableData) obj;
+		return items.equals(other.items);
 	}
 
 	@Override
