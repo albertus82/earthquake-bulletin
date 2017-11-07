2b72e93f172c0c1541d966ca427f152626e718a5
Alberto <albertus82@gmail.com>
2017-11-07 21:07:47 +0100

    Images

--- src/main/java/it/albertus/eqbulletin/gui/Images.java
+++ src/main/java/it/albertus/eqbulletin/gui/Images.java
@@ -1,37 +1,44 @@
 package it.albertus.eqbulletin.gui;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.Collection;
+import java.util.LinkedHashSet;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.ImageData;
 import org.eclipse.swt.graphics.ImageLoader;
 import org.eclipse.swt.widgets.Display;
 
-import it.albertus.util.IOUtils;
+import it.albertus.util.logging.LoggerFactory;
 
 public class Images {
 
-	/* Icona principale dell'applicazione (in vari formati) */
-	private static final Image[] MAIN_ICONS = loadIcons("main.ico");
+	private static final Logger logger = LoggerFactory.getLogger(Images.class);
+
+	// Main application icon (in various formats)
+	private static final Collection<Image> mainIcons = new LinkedHashSet<>();
 
 	private Images() {
 		throw new IllegalAccessError();
 	}
 
-	private static Image[] loadIcons(final String fileName) {
-		final InputStream is = Images.class.getResourceAsStream(fileName);
-		final ImageData[] images = new ImageLoader().load(is);
-		IOUtils.closeQuietly(is);
-		final Image[] icons = new Image[images.length];
-		int i = 0;
-		for (final ImageData id : images) {
-			icons[i++] = new Image(Display.getCurrent(), id);
+	static {
+		try (final InputStream is = Images.class.getResourceAsStream("main.ico")) {
+			final ImageData[] images = new ImageLoader().load(is);
+			for (final ImageData id : images) {
+				mainIcons.add(new Image(Display.getCurrent(), id));
+			}
 		}
-		return icons;
+		catch (final IOException e) {
+			logger.log(Level.WARNING, e.toString(), e);
+		}
 	}
 
 	public static Image[] getMainIcons() {
-		return MAIN_ICONS;
+		return mainIcons.toArray(new Image[mainIcons.size()]);
 	}
 
 }
