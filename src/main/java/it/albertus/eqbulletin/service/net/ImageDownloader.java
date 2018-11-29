a5dc01dbd4b2ebd5a5241c60d0876ff78fe4d274
Alberto <albertus82@users.noreply.github.com>
2018-11-29 22:53:29 +0100

    Fixed map download from RSS feed.

--- src/main/java/it/albertus/eqbulletin/service/net/ImageDownloader.java
+++ src/main/java/it/albertus/eqbulletin/service/net/ImageDownloader.java
@@ -3,8 +3,14 @@ package it.albertus.eqbulletin.service.net;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.Serializable;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.zip.GZIPInputStream;
 
 import it.albertus.eqbulletin.config.EarthquakeBulletinConfig;
@@ -12,10 +18,17 @@ import it.albertus.eqbulletin.gui.preference.Preference;
 import it.albertus.eqbulletin.model.MapImage;
 import it.albertus.jface.preference.IPreferencesConfiguration;
 import it.albertus.util.IOUtils;
+import it.albertus.util.logging.LoggerFactory;
 
 public class ImageDownloader {
 
-	private static final int BUFFER_SIZE = 8192;
+	private static final short BUFFER_SIZE = 8192;
+
+	private static final Logger logger = LoggerFactory.getLogger(ImageDownloader.class);
+
+	private static final Collection<Integer> httpRedirectionResponseCodes = Collections.unmodifiableList(Arrays.asList(HttpURLConnection.HTTP_MOVED_PERM, HttpURLConnection.HTTP_MOVED_TEMP));
+
+	private static final byte REDIRECTION_LIMIT = 20;
 
 	private static final IPreferencesConfiguration configuration = EarthquakeBulletinConfig.getInstance();
 
@@ -24,14 +37,9 @@ public class ImageDownloader {
 	}
 
 	public static MapImage downloadImage(final URL url, final String etag) throws IOException {
-		HttpURLConnection urlConnection = null;
+		final HttpURLConnection urlConnection = connect(url, etag);
 		try {
-			urlConnection = prepareConnection(url);
-			if (etag != null && !etag.isEmpty()) {
-				urlConnection.setReadTimeout(Math.min(3000, configuration.getInt(Preference.HTTP_READ_TIMEOUT_MS, ConnectionFactory.Defaults.READ_TIMEOUT_IN_MILLIS)));
-				urlConnection.setRequestProperty("If-None-Match", etag);
-			}
-			if (urlConnection.getResponseCode() == HttpURLConnection.HTTP_NOT_MODIFIED) { // Connection starts here
+			if (urlConnection.getResponseCode() == HttpURLConnection.HTTP_NOT_MODIFIED) {
 				return null;
 			}
 			else {
@@ -44,16 +52,54 @@ public class ImageDownloader {
 			}
 		}
 		finally {
-			if (urlConnection != null) {
-				urlConnection.disconnect();
-			}
+			urlConnection.disconnect();
 		}
 	}
 
-	private static HttpURLConnection prepareConnection(final URL url) throws IOException {
+	private static HttpURLConnection connect(URL url, final String etag) throws IOException {
+		HttpURLConnection urlConnection = null;
+		try {
+			urlConnection = prepareConnection(url, etag);
+			byte redirectionCounter = 0;
+			while (httpRedirectionResponseCodes.contains(urlConnection.getResponseCode())) { // Connection starts here
+				final String location = urlConnection.getHeaderField("Location");
+				urlConnection.disconnect();
+				if (location == null || location.isEmpty()) {
+					throw new IllegalStateException("URL \"" + url + "\": server responded with HTTP " + urlConnection.getResponseCode() + " omitting the \"Location\" header.");
+				}
+				if (redirectionCounter++ >= REDIRECTION_LIMIT) {
+					throw new IllegalStateException("The page \"" + url + "\" isn't redirecting properly.");
+				}
+				final String spec;
+				if (location.startsWith("/")) { // Relative
+					final String baseUrl = url.getProtocol() + "://" + url.getHost() + (url.getPort() != -1 ? ":" + url.getPort() : "");
+					spec = baseUrl + location;
+				}
+				else { // Absolute
+					spec = location;
+				}
+				logger.log(Level.FINE, "Redirecting from \"{0}\" to \"{1}\"", new Serializable[] { url, spec });
+				url = new URL(spec);
+				urlConnection = prepareConnection(url, etag);
+			}
+			return urlConnection;
+		}
+		catch (final Exception e) {
+			if (urlConnection != null) {
+				urlConnection.disconnect();
+			}
+			throw e;
+		}
+	}
+
+	private static HttpURLConnection prepareConnection(final URL url, final String etag) throws IOException {
 		final HttpURLConnection urlConnection = ConnectionFactory.createHttpConnection(url);
 		urlConnection.setRequestProperty("Accept", "image/*,*/*;0.9");
 		urlConnection.setRequestProperty("Accept-Encoding", "gzip");
+		if (etag != null && !etag.isEmpty()) {
+			urlConnection.setReadTimeout(Math.min(3000, configuration.getInt(Preference.HTTP_READ_TIMEOUT_MS, ConnectionFactory.Defaults.READ_TIMEOUT_IN_MILLIS)));
+			urlConnection.setRequestProperty("If-None-Match", etag);
+		}
 		return urlConnection;
 	}
 
