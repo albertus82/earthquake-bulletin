699685fc30d3a809b13a2bdcdfddac47cf3baff7
Alberto <albertus82@users.noreply.github.com>
2018-12-27 19:37:29 +0100

    Principle of least privilege

--- src/main/java/it/albertus/eqbulletin/model/Coordinate.java
+++ src/main/java/it/albertus/eqbulletin/model/Coordinate.java
@@ -3,17 +3,17 @@ package it.albertus.eqbulletin.model;
 import java.text.NumberFormat;
 import java.util.Locale;
 
-public abstract class Coordinate extends FloatValue {
+abstract class Coordinate extends FloatValue {
 
 	private static final long serialVersionUID = 7312923076881011764L;
 
 	static final char DEGREE_SIGN = '\u00B0';
 
-	public Coordinate(final float value) {
+	Coordinate(final float value) {
 		super(value);
 	}
 
-	protected static final ThreadLocal<NumberFormat> numberFormats = ThreadLocal.withInitial(() -> {
+	static final ThreadLocal<NumberFormat> numberFormats = ThreadLocal.withInitial(() -> {
 		final NumberFormat nf = NumberFormat.getNumberInstance(Locale.US);
 		nf.setMinimumFractionDigits(2);
 		nf.setMaximumFractionDigits(2);
