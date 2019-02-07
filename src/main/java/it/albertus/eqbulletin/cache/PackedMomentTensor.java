bc37c8f5d13c883f71bc7dfd154588c1d7af6301
Alberto <albertus82@users.noreply.github.com>
2019-02-07 19:39:00 +0100

    Refactoring.

--- src/main/java/it/albertus/eqbulletin/cache/PackedMomentTensor.java
+++ src/main/java/it/albertus/eqbulletin/cache/PackedMomentTensor.java
@@ -1,26 +1,23 @@
 package it.albertus.eqbulletin.cache;
 
-import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOError;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.io.Serializable;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.zip.DeflaterOutputStream;
-import java.util.zip.InflaterInputStream;
+import java.util.zip.InflaterOutputStream;
 
 import it.albertus.eqbulletin.model.MomentTensor;
-import it.albertus.util.IOUtils;
 import it.albertus.util.logging.LoggerFactory;
 
 class PackedMomentTensor implements Serializable {
 
-	private static final long serialVersionUID = -1469703423164162817L;
-
-	private static final int BUFFER_SIZE = 512;
+	private static final long serialVersionUID = 6571208579436743965L;
 
 	private static final Logger logger = LoggerFactory.getLogger(PackedMomentTensor.class);
 
@@ -38,8 +35,8 @@ class PackedMomentTensor implements Serializable {
 		final String text = momentTensor.getText();
 		logger.log(Level.FINE, "Original text.length() = {0,number,#} chars.", text.length());
 		try (final ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
-			try (final DeflaterOutputStream dos = new DeflaterOutputStream(baos)) {
-				dos.write(text.getBytes(charset));
+			try (final OutputStream os = new DeflaterOutputStream(baos)) {
+				os.write(text.getBytes(charset));
 			} // The stream is finished and closed automatically.
 			final byte[] bytes = baos.toByteArray();
 			logger.log(Level.FINE, "Compressed bytes.length = {0,number,#} bytes.", bytes.length);
@@ -52,8 +49,10 @@ class PackedMomentTensor implements Serializable {
 
 	MomentTensor unpack() {
 		logger.log(Level.FINE, "Compressed bytes.length = {0,number,#} bytes.", bytes.length);
-		try (final ByteArrayInputStream bais = new ByteArrayInputStream(bytes); final InflaterInputStream dis = new InflaterInputStream(bais); final ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
-			IOUtils.copy(dis, baos, BUFFER_SIZE);
+		try (final ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
+			try (final OutputStream os = new InflaterOutputStream(baos)) {
+				os.write(bytes);
+			}
 			final String text = baos.toString(charset.name());
 			logger.log(Level.FINE, "Expanded text.length() = {0,number,#} chars.", text.length());
 			return new MomentTensor(text, etag);
