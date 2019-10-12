17491ad1dce1b13d1e3c2081725a902027fe6518
Alberto <albertus82@users.noreply.github.com>
2019-10-12 20:31:27 +0200

    BeachBall serialVersionUID

--- src/main/java/it/albertus/eqbulletin/model/BeachBall.java
+++ src/main/java/it/albertus/eqbulletin/model/BeachBall.java
@@ -4,7 +4,7 @@ import it.albertus.eqbulletin.service.net.StaticBinaryResource;
 
 public class BeachBall extends StaticBinaryResource {
 
-	private static final long serialVersionUID = 4147911522978502502L;
+	private static final long serialVersionUID = 4459492307313800868L;
 
 	public BeachBall(final byte[] bytes, final String etag) {
 		super(bytes, etag);
