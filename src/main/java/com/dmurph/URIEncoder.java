bae64b54fd0a07584c6e2fda12f545f0a0dc09ed
Alberto <albertus82@users.noreply.github.com>
2021-03-07 11:24:58 +0100

    Optimized URIEncoder

--- src/main/java/com/dmurph/URIEncoder.java
+++ src/main/java/com/dmurph/URIEncoder.java
@@ -2,6 +2,7 @@ package com.dmurph;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 /**
  * Simple uri encoder, made from the spec at http://www.ietf.org/rfc/rfc2396.txt
@@ -16,12 +17,11 @@ public class URIEncoder {
 	private static final String MARK = "-_.!~*'()\"";
 	private static final char[] hex = "0123456789ABCDEF".toCharArray();
 
-	public static String encodeURI(String argString) {
-		StringBuilder uri = new StringBuilder();
+	public static String encodeURI(@NonNull final CharSequence argString) {
+		final StringBuilder uri = new StringBuilder();
 
-		char[] chars = argString.toCharArray();
-		for (int i = 0; i < chars.length; i++) {
-			char c = chars[i];
+		for (int i = 0; i < argString.length(); i++) {
+			final char c = argString.charAt(i);
 			if ((c >= '0' && c <= '9') || (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || MARK.indexOf(c) != -1) {
 				uri.append(c);
 			}
@@ -32,21 +32,19 @@ public class URIEncoder {
 		return uri.toString();
 	}
 
-	private static void appendEscaped(StringBuilder uri, char c) {
+	private static void appendEscaped(final StringBuilder uri, final char c) {
 		if (c <= (char) 0xF) {
-			uri.append("%");
-			uri.append('0');
+			uri.append("%0");
 			uri.append(hex[c]);
 		}
 		else if (c <= (char) 0xFF) {
-			uri.append("%");
+			uri.append('%');
 			uri.append(hex[c >> 8]);
 			uri.append(hex[c & 0xF]);
 		}
 		else {
 			// unicode
-			uri.append('\\');
-			uri.append('u');
+			uri.append("\\u");
 			uri.append(hex[c >> 24]);
 			uri.append(hex[(c >> 16) & 0xF]);
 			uri.append(hex[(c >> 8) & 0xF]);
