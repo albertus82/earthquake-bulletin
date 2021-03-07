e579f2aab42e2e6a8f350a44dcdbda75931b408a
Alberto <albertus82@users.noreply.github.com>
2021-03-07 13:32:43 +0100

    Refactoring.

--- src/main/java/it/albertus/eqbulletin/service/decode/html/HtmlBulletinDecoder.java
+++ src/main/java/it/albertus/eqbulletin/service/decode/html/HtmlBulletinDecoder.java
@@ -25,6 +25,7 @@ import it.albertus.eqbulletin.model.Status;
 import it.albertus.eqbulletin.service.GeofonUtils;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class HtmlBulletinDecoder {
@@ -54,52 +55,84 @@ public class HtmlBulletinDecoder {
 		}
 	}
 
-	private static Earthquake decodeItem(final Element anchor) {
+	private static Earthquake decodeItem(@NonNull final Element anchor) {
 		try {
 			final Elements divs = anchor.getElementsByTag("div");
 			final Elements spans = anchor.getElementsByTag("span");
 
-			final ZonedDateTime time = dateTimeFormatter.parse(divs.get(6).textNodes().get(0).text().trim(), ZonedDateTime::from);
-
-			final String guid = anchor.attr("href").substring(anchor.attr("href").indexOf('=') + 1).trim();
-			final URI link = GeofonUtils.toURI(anchor.absUrl("href"));
-
-			final float magnitude = Float.parseFloat(spans.first().text());
-
-			final String[] lonLat = divs.get(4).attr("title").split(",");
-
-			final String[] splitLat = lonLat[1].split(DEGREE_SIGN);
-			float latitude = Float.parseFloat(splitLat[0]);
-			if ("S".equalsIgnoreCase(splitLat[1])) {
-				latitude = -latitude;
-			}
-
-			final String[] splitLon = lonLat[0].split(DEGREE_SIGN);
-			float longitude = Float.parseFloat(splitLon[0]);
-			if ("W".equalsIgnoreCase(splitLon[1])) {
-				longitude = -longitude;
-			}
-
-			final Optional<Element> depthSpan = spans.stream().filter(e -> e.hasClass("pull-right")).findFirst();
-			final short depth = Short.parseShort(depthSpan.orElseThrow(() -> new IllegalArgumentException(String.valueOf(spans))).text().replace("*", "").trim());
+			final ZonedDateTime time = decodeTime(divs);
+			final String guid = decodeGuid(anchor);
+			final URI link = decodeLink(anchor);
+			final float magnitude = decodeMagnitude(spans);
+			float[] lonLat = decodeLonLat(divs);
+			final short depth = decodeDepth(spans);
 			final Status status = null; // the status was sadly removed from the HTML page on 2019-10-08
-			final String region = divs.get(4).text();
+			final String region = decodeRegion(divs);
+			final int year = time.get(ChronoField.YEAR);
+			final URI enclosureUri = decodeEnclosureUri(guid, year);
+			final URI momentTensorUri = decodeMomentTensorUri(anchor, guid, year);
 
-			final URI enclosureUri = GeofonUtils.getEventMapUri(guid, time.get(ChronoField.YEAR));
-
-			URI momentTensorUri = null;
-			for (final Element img : anchor.getElementsByTag("img")) {
-				if ("MT".equalsIgnoreCase(img.attr("alt"))) {
-					momentTensorUri = GeofonUtils.getEventMomentTensorUri(guid, time.get(ChronoField.YEAR));
-					break;
-				}
-			}
-
-			return new Earthquake(guid, time, magnitude, Latitude.valueOf(latitude), Longitude.valueOf(longitude), Depth.valueOf(depth), status, region, link, enclosureUri, momentTensorUri);
+			return new Earthquake(guid, time, magnitude, Latitude.valueOf(lonLat[1]), Longitude.valueOf(lonLat[0]), Depth.valueOf(depth), status, region, link, enclosureUri, momentTensorUri);
 		}
 		catch (final Exception e) {
 			throw new IllegalArgumentException(anchor.toString(), e);
 		}
+	}
+
+	private static ZonedDateTime decodeTime(@NonNull final Elements divs) {
+		return dateTimeFormatter.parse(divs.get(6).textNodes().get(0).text().trim(), ZonedDateTime::from);
+	}
+
+	private static String decodeGuid(@NonNull final Element anchor) {
+		return anchor.attr("href").substring(anchor.attr("href").indexOf('=') + 1).trim();
+	}
+
+	private static URI decodeLink(@NonNull final Element anchor) {
+		return GeofonUtils.toURI(anchor.absUrl("href"));
+	}
+
+	private static float decodeMagnitude(@NonNull final Elements spans) {
+		return Float.parseFloat(spans.first().text());
+	}
+
+	private static float[] decodeLonLat(@NonNull final Elements divs) {
+		final String[] lonLat = divs.get(4).attr("title").split(",");
+
+		final String[] splitLat = lonLat[1].split(DEGREE_SIGN);
+		float latitude = Float.parseFloat(splitLat[0]);
+		if ("S".equalsIgnoreCase(splitLat[1])) {
+			latitude = -latitude;
+		}
+
+		final String[] splitLon = lonLat[0].split(DEGREE_SIGN);
+		float longitude = Float.parseFloat(splitLon[0]);
+		if ("W".equalsIgnoreCase(splitLon[1])) {
+			longitude = -longitude;
+		}
+
+		return new float[] { longitude, latitude };
+	}
+
+	private static short decodeDepth(@NonNull final Elements spans) {
+		final Optional<Element> depthSpan = spans.stream().filter(e -> e.hasClass("pull-right")).findFirst();
+		return Short.parseShort(depthSpan.orElseThrow(() -> new IllegalArgumentException(String.valueOf(spans))).text().replace("*", "").trim());
+	}
+
+	private static String decodeRegion(@NonNull final Elements divs) {
+		return divs.get(4).text();
+	}
+
+	private static URI decodeEnclosureUri(@NonNull final String guid, final int year) {
+		return GeofonUtils.getEventMapUri(guid, year);
+	}
+
+	private static URI decodeMomentTensorUri(@NonNull final Element anchor, @NonNull final String guid, final int year) {
+		for (final Element img : anchor.getElementsByTag("img")) {
+			if ("MT".equalsIgnoreCase(img.attr("alt"))) {
+				return GeofonUtils.getEventMomentTensorUri(guid, year);
+			}
+		}
+		return null;
 	}
 
 }
