5a8eba7095e9ea8e29bf1abfe2879a1aef51f776
Alberto <albertus82@users.noreply.github.com>
2021-03-09 21:51:57 +0100

    Refactoring.

--- src/main/java/it/albertus/eqbulletin/gui/LocalizedWidgets.java
+++ src/main/java/it/albertus/eqbulletin/gui/LocalizedWidgets.java
@@ -16,40 +16,37 @@ public class LocalizedWidgets implements Map<Widget, ISupplier<String>> {
 
 	private static final Map<Class<? extends Widget>, Method> setTextMethods = new HashMap<Class<? extends Widget>, Method>();
 
-	private final Map<Widget, ISupplier<String>> wrapped;
+	private final Map<Widget, ISupplier<String>> wrappedMap;
 
 	/** @see HashMap#HashMap() */
 	public LocalizedWidgets() {
-		wrapped = new HashMap<Widget, ISupplier<String>>();
+		wrappedMap = new HashMap<Widget, ISupplier<String>>();
 	}
 
 	/** @see HashMap#HashMap(int) */
 	public LocalizedWidgets(final int initialCapacity) {
-		wrapped = new HashMap<Widget, ISupplier<String>>(initialCapacity);
+		wrappedMap = new HashMap<Widget, ISupplier<String>>(initialCapacity);
 	}
 
 	public void updateTexts() {
-		for (final Entry<Widget, ISupplier<String>> entry : wrapped.entrySet()) {
+		for (final Entry<Widget, ISupplier<String>> entry : wrappedMap.entrySet()) {
 			resetText(entry.getKey(), entry.getValue());
 		}
 	}
 
-	/** @see Map#put(Object, Object) */
 	@Override
 	public ISupplier<String> put(final Widget widget, final ISupplier<String> supplier) {
 		final Class<? extends Widget> widgetClass = widget.getClass();
-		Method method = setTextMethods.get(widgetClass);
-		if (method == null) {
+		if (!setTextMethods.containsKey(widgetClass)) {
 			try {
-				method = widgetClass.getMethod("setText", String.class);
-				setTextMethods.put(widgetClass, method);
+				setTextMethods.put(widgetClass, widgetClass.getMethod("setText", String.class));
 			}
 			catch (final NoSuchMethodException e) {
 				throw new IllegalArgumentException(String.valueOf(widget), e);
 			}
 		}
 		resetText(widget, supplier);
-		return wrapped.put(widget, supplier);
+		return wrappedMap.put(widget, supplier);
 	}
 
 	@Override
@@ -61,52 +58,52 @@ public class LocalizedWidgets implements Map<Widget, ISupplier<String>> {
 
 	@Override
 	public int size() {
-		return wrapped.size();
+		return wrappedMap.size();
 	}
 
 	@Override
 	public boolean isEmpty() {
-		return wrapped.isEmpty();
+		return wrappedMap.isEmpty();
 	}
 
 	@Override
 	public boolean containsKey(final Object key) {
-		return wrapped.containsKey(key);
+		return wrappedMap.containsKey(key);
 	}
 
 	@Override
 	public boolean containsValue(final Object value) {
-		return wrapped.containsValue(value);
+		return wrappedMap.containsValue(value);
 	}
 
 	@Override
 	public ISupplier<String> get(final Object key) {
-		return wrapped.get(key);
+		return wrappedMap.get(key);
 	}
 
 	@Override
 	public ISupplier<String> remove(final Object key) {
-		return wrapped.remove(key);
+		return wrappedMap.remove(key);
 	}
 
 	@Override
 	public void clear() {
-		wrapped.clear();
+		wrappedMap.clear();
 	}
 
 	@Override
 	public Set<Widget> keySet() {
-		return wrapped.keySet();
+		return wrappedMap.keySet();
 	}
 
 	@Override
 	public Collection<ISupplier<String>> values() {
-		return wrapped.values();
+		return wrappedMap.values();
 	}
 
 	@Override
 	public Set<Entry<Widget, ISupplier<String>>> entrySet() {
-		return wrapped.entrySet();
+		return wrappedMap.entrySet();
 	}
 
 	public <T extends Widget> Entry<T, ISupplier<String>> putAndReturn(final T widget, final ISupplier<String> supplier) {
@@ -115,7 +112,7 @@ public class LocalizedWidgets implements Map<Widget, ISupplier<String>> {
 	}
 
 	private static void resetText(final Widget widget, final ISupplier<String> supplier) {
-		if (widget != null && !widget.isDisposed()) {
+		if (supplier != null && widget != null && !widget.isDisposed()) {
 			try {
 				setTextMethods.get(widget.getClass()).invoke(widget, String.valueOf(supplier.get()));
 			}
