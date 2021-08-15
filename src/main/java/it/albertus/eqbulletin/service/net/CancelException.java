da103ea2af678f0fa5ecb78c5b0bba5fd469ed18
Alberto <albertus82@users.noreply.github.com>
2021-08-15 08:36:09 +0200

    Refactoring.

--- src/main/java/it/albertus/eqbulletin/service/net/CancelException.java
+++ src/main/java/it/albertus/eqbulletin/service/net/CancelException.java
@@ -1,10 +1,11 @@
 package it.albertus.eqbulletin.service.net;
 
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
 class CancelException extends Exception {
 
 	private static final long serialVersionUID = -5082561084350306737L;
-
-	CancelException() {}
 
 	CancelException(final Throwable cause) {
 		super(cause);
