b94456f310dd812a3c32618cd9d4c83edda95c2f
Alberto <albertus82@users.noreply.github.com>
2021-09-18 07:53:09 +0200

    Improved error handling.

--- src/main/java/it/albertus/eqbulletin/util/BuildInfo.java
+++ src/main/java/it/albertus/eqbulletin/util/BuildInfo.java
@@ -1,5 +1,6 @@
 package it.albertus.eqbulletin.util;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.UncheckedIOException;
@@ -11,10 +12,15 @@ public enum BuildInfo {
 
 	INSTANCE;
 
+	private static final String RESOURCE_NAME = "/META-INF/build-info.properties";
+
 	private final Properties properties = new Properties();
 
 	private BuildInfo() {
-		try (final InputStream is = getClass().getResourceAsStream("/META-INF/build-info.properties")) {
+		try (final InputStream is = getClass().getResourceAsStream(RESOURCE_NAME)) {
+			if (is == null) {
+				throw new FileNotFoundException(RESOURCE_NAME);
+			}
 			properties.load(is);
 		}
 		catch (final IOException e) {
