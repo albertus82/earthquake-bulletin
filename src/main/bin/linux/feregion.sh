130a9aa65ee214714f2fb29e1a74e01f1cdcc6f1
Alberto <albertus82@users.noreply.github.com>
2021-09-24 20:23:23 +0200

    Removed useless classpath part.

--- src/main/bin/linux/feregion.sh
+++ src/main/bin/linux/feregion.sh
@@ -10,4 +10,4 @@ while [ -h "$PRG" ] ; do
   fi
 done
 PRGDIR=`dirname "$PRG"`
-java -Xms${feregion.vm.initialHeapSize}m -Xmx${feregion.vm.maxHeapSize}m -cp "$PRGDIR/${project.build.finalName}.${project.packaging}:$PRGDIR/lib/*" ${feregion.mainClass} $1 $2 $3
+java -Xms${feregion.vm.initialHeapSize}m -Xmx${feregion.vm.maxHeapSize}m -cp "$PRGDIR/${project.build.finalName}.${project.packaging}" ${feregion.mainClass} $1 $2 $3
